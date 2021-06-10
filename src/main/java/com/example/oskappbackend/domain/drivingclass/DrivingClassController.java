d78d8d3a7a455c531a63bf3782af8dbc16ea2432
premek1996 <przemyslaw.jambor@gmail.com>
2021-06-10 13:33:42 +0200

    Some changes

--- src/main/java/com/example/oskappbackend/domain/drivingclass/DrivingClassController.java
+++ src/main/java/com/example/oskappbackend/domain/drivingclass/DrivingClassController.java
@@ -2,11 +2,12 @@ package com.example.oskappbackend.domain.drivingclass;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/driving-classes")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class DrivingClassController {
 
     private final DrivingClassService drivingClassService;
@@ -43,7 +45,7 @@ public class DrivingClassController {
         return drivingClassService.createDrivingClass(DrivingClassMapper.from(drivingClassDTO));
     }
 
-    @PutMapping("/{id}")
+    @PostMapping("/{id}")
     public DrivingClass updateDrivingClass(@PathVariable Long id,
                                            @RequestBody List<Location> locations) {
         return drivingClassService.updateDrivingClass(id, locations)
